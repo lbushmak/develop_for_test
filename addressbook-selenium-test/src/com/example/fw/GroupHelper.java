3260588ee72e4879f0a7adbc2c8de4151f0a8e84
lbushmak <liubov.bushmakina@gmail.com>
2015-10-26 15:40:09 +0300

    lesson3_hw1

--- addressbook-selenium-test/src/com/example/fw/GroupHelper.java
+++ addressbook-selenium-test/src/com/example/fw/GroupHelper.java
@@ -1,6 +1,10 @@
 package com.example.fw;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import com.example.tests.GroupData;
 
@@ -39,20 +43,30 @@ public class GroupHelper extends HelperBase {
 	}
 
 	private void selectGroupByIndex(int index) {
-		click(By.xpath("//input[@name='selected[]'][" + index + "]"));
+		click(By.xpath("//input[@name='selected[]'][" + (index + 1) + "]"));
 	}
 
 	public void initGroupModification(int index) {
 		selectGroupByIndex(index);
 		click(By.name("edit"));
-		// TODO Auto-generated method stub
 
 	}
 
 	public void submitGroupModification() {
 		click(By.name("update"));
-		// TODO Auto-generated method stub
 
 	}
 
+	public List<GroupData> getGroups() {
+		List<GroupData> groups = new ArrayList<GroupData>();
+		List<WebElement> checkboxes = driver.findElements(By.name("selected[]"));
+		for (WebElement checkbox : checkboxes) {
+			GroupData group = new GroupData();
+			String title = checkbox.getAttribute("title");
+			group.groupname = title.substring("Select (".length(), title.length() - ")".length());
+			groups.add(group);
+		}
+		return groups;
+
+	}
 }
--- addressbook-selenium-test/src/com/example/tests/GroupCreationTests.java
+++ addressbook-selenium-test/src/com/example/tests/GroupCreationTests.java
@@ -1,30 +1,35 @@
 package com.example.tests;
 
+import static org.testng.Assert.assertEquals;
+
+import java.util.Collections;
+import java.util.List;
+
 import org.testng.annotations.Test;
 
 public class GroupCreationTests extends TestBase {
 
-	@Test
-	public void testNonEmptyGroupCreation() throws Exception {
+	@Test(dataProvider = "randomValidGroupGenerator")
+	public void testGroupCreationWithValidData(GroupData group) throws Exception {
 		app.getNavigationHelper().openMainPage();
 		app.getNavigationHelper().gotoGroupsPage();
+
+		// save old state
+		List<GroupData> oldList = app.getGroupHelper().getGroups();
+
+		// actions
 		app.getGroupHelper().initGroupCreation();
-		GroupData group = new GroupData();
-		group.groupname = "group_1";
-		group.header = "header_1";
-		group.footer = "footer_1";
 		app.getGroupHelper().fillGroupForm(group);
 		app.getGroupHelper().submitGroupCreation();
 		app.getNavigationHelper().gotoGroupsPage();
-	}
 
-	@Test
-	public void testEmptyGroupCreation() throws Exception {
-		app.getNavigationHelper().openMainPage();
-		app.getNavigationHelper().gotoGroupsPage();
-		app.getGroupHelper().initGroupCreation();
-		app.getGroupHelper().fillGroupForm(new GroupData("", "", ""));
-		app.getGroupHelper().submitGroupCreation();
-		app.getNavigationHelper().gotoGroupsPage();
+		// save new state
+		List<GroupData> newList = app.getGroupHelper().getGroups();
+
+		// compare states
+		oldList.add(group);
+		Collections.sort(oldList);
+		assertEquals(newList, oldList);
+
 	}
 }
