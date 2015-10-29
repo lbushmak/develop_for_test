298aad7a217cfb1ecf09b33bfa5b9154b0d7e536
lbushmak <liubov.bushmakina@gmail.com>
2015-10-29 15:28:56 +0300

    lesson3_hw_5_7

--- addressbook-selenium-test/src/com/example/tests/TestBase.java
+++ addressbook-selenium-test/src/com/example/tests/TestBase.java
@@ -43,13 +43,82 @@ public class TestBase {
 		return list.iterator();
 	}
 
+	@DataProvider
+	public Iterator<Object[]> randomvalidContactGenerator() {
+		List<Object[]> list = new ArrayList<Object[]>();
+
+		for (int i = 0; i < 5; i++) {
+			ContactData contact = new ContactData();
+			contact.firstname = generateRandomString();
+			contact.lastname = generateRandomString();
+			contact.address = generateRandomString();
+			contact.address2 = generateRandomString();
+			contact.email = generateRandomString();
+			contact.email2 = generateRandomString();
+			contact.home = generateRandomString();
+			contact.mobile = generateRandomString();
+			contact.phone2 = generateRandomString();
+			contact.work = generateRandomString();
+
+			contact.bday = generateRandomBDay();
+			contact.byear = generateRandomBYear();
+			contact.bmonth = generateRandomBMonth();
+
+			contact.group = chooseRandomGroup();
+			list.add(new Object[] { contact });
+		}
+		return list.iterator();
+	}
+
+	public GroupData chooseRandomGroup() {
+
+		app.getNavigationHelper().getGroupList();
+		List<GroupData> groupList = app.getGroupHelper().getGroups();
+		Random rnd = new Random();// initiate random value
+		int groupIndex = rnd.nextInt(groupList.size() + 1); // get random value
+															// from
+															// 0 to list size
+
+		return groupList.get(groupIndex); // get random group element
+	}
+
 	public String generateRandomString() {
 		Random rnd = new Random();
-		if (rnd.nextInt(3) == 0) {
+		if (rnd.nextInt(5) == 0) {
 			return "";
 		} else {
 			return "test" + rnd.nextInt();
 		}
 
 	}
+
+	public String generateRandomBDay() {
+		Random rnd = new Random();
+		if (rnd.nextInt(3) == 0) {
+			return "-";
+		} else {
+			return String.valueOf(rnd.nextInt(31) + 1);
+		}
+	}
+
+	public String generateRandomBMonth() {
+		String[] month = new String[] { "January", "February", "March", "April", "May", "June", "July", "August",
+				"September", "November", "December" };
+		Random rnd = new Random();
+		if (rnd.nextInt(3) == 0) {
+			return "-";
+		} else {
+			return month[rnd.nextInt(month.length)];
+		}
+	}
+
+	public String generateRandomBYear() {
+		Random rnd = new Random();
+		if (rnd.nextInt(3) == 0) {
+			return "";
+		} else {
+			return String.valueOf(rnd.nextInt(1015) + 1001);
+		}
+	}
+
 }
